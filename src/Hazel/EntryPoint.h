10b6bc97e6bfeb39136a9bfd8461f4ec80ea6f5f
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2024-02-10 14:04:55 -0500

    Add logging system

--- src/Hazel/EntryPoint.h
+++ src/Hazel/EntryPoint.h
@@ -6,7 +6,11 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 	int main(int argc, char ** argv) {
+		int a = 5;
 		printf("Hazel Engine\n");
+		Hazel::Log::Init();
+		HZ_CORE_WARN("Initialized Core Log");
+		HZ_CLIENT_ERROR("Hello! Var={0}", a);
 		auto app = Hazel::CreateApplication();
 		app->Run();
 		delete app;
